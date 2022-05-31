3d4c6d2f888f7c42c728076a979ca15a344899ae
nofarb <nofarb@gmail.com>
2022-05-30 20:43:37 -0700

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -19,7 +19,102 @@ def test_get_all_books():
     assert res[1]['author'] == 'Will'
     assert response.status_code == 200
     assert type(res) is list
+    
+@pytest.mark.get_request
+def test_get_all_books1():
+    response = app.test_client().get('/bookapi/books')
 
+    res = json.loads(response.data.decode('utf-8')).get("Books")
+    assert type(res[0]) is dict
+    assert type(res[1]) is dict
+    assert res[0]['author'] == 'Havard'
+    assert res[1]['author'] == 'Will'
+    assert response.status_code == 200
+    assert type(res) is list
+    
+@pytest.mark.get_request
+def test_get_all_books2():
+    response = app.test_client().get('/bookapi/books')
+
+    res = json.loads(response.data.decode('utf-8')).get("Books")
+    assert type(res[0]) is dict
+    assert type(res[1]) is dict
+    assert res[0]['author'] == 'Havard'
+    assert res[1]['author'] == 'Will'
+    assert response.status_code == 200
+    assert type(res) is list
+
+@pytest.mark.get_request
+def test_get_all_books3():
+    response = app.test_client().get('/bookapi/books')
+
+    res = json.loads(response.data.decode('utf-8')).get("Books")
+    assert type(res[0]) is dict
+    assert type(res[1]) is dict
+    assert res[0]['author'] == 'Havard'
+    assert res[1]['author'] == 'Will'
+    assert response.status_code == 200
+    assert type(res) is list
+
+@pytest.mark.get_request
+def test_get_all_books4():
+    response = app.test_client().get('/bookapi/books')
+
+    res = json.loads(response.data.decode('utf-8')).get("Books")
+    assert type(res[0]) is dict
+    assert type(res[1]) is dict
+    assert res[0]['author'] == 'Havard'
+    assert res[1]['author'] == 'Will'
+    assert response.status_code == 200
+    assert type(res) is list
+
+@pytest.mark.get_request
+def test_get_all_books5():
+    response = app.test_client().get('/bookapi/books')
+
+    res = json.loads(response.data.decode('utf-8')).get("Books")
+    assert type(res[0]) is dict
+    assert type(res[1]) is dict
+    assert res[0]['author'] == 'Havard'
+    assert res[1]['author'] == 'Will'
+    assert response.status_code == 200
+    assert type(res) is list
+
+@pytest.mark.get_request
+def test_get_all_books6():
+    response = app.test_client().get('/bookapi/books')
+
+    res = json.loads(response.data.decode('utf-8')).get("Books")
+    assert type(res[0]) is dict
+    assert type(res[1]) is dict
+    assert res[0]['author'] == 'Havard'
+    assert res[1]['author'] == 'Will'
+    assert response.status_code == 200
+    assert type(res) is list
+    
+@pytest.mark.get_request
+def test_get_all_books7():
+    response = app.test_client().get('/bookapi/books')
+
+    res = json.loads(response.data.decode('utf-8')).get("Books")
+    assert type(res[0]) is dict
+    assert type(res[1]) is dict
+    assert res[0]['author'] == 'Havard'
+    assert res[1]['author'] == 'Will'
+    assert response.status_code == 200
+    assert type(res) is list
+
+@pytest.mark.get_request
+def test_get_all_books8():
+    response = app.test_client().get('/bookapi/books')
+
+    res = json.loads(response.data.decode('utf-8')).get("Books")
+    assert type(res[0]) is dict
+    assert type(res[1]) is dict
+    assert res[0]['author'] == 'Havard'
+    assert res[1]['author'] == 'Will'
+    assert response.status_code == 200
+    assert type(res) is list
 
 @pytest.mark.get_request
 def test_get_book_by_id():
